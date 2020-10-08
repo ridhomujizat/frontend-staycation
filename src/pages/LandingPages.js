02fbca0edf39c3fafc63b730371b553828a1bd40
ridhomujizat <ridhomujizat@gmail.com>
2020-10-08 22:40:11 +0700

    Created Header

--- src/pages/LandingPages.js
+++ src/pages/LandingPages.js
@@ -1,7 +1,12 @@
 import React, { Component } from "react";
-import moduleName from "elements/Buttons";
+import Header from "parts/Header";
+
 export default class LandingPages extends Component {
   render() {
-    return <div></div>;
+    return (
+      <>
+        <Header {...this.props}></Header>;
+      </>
+    );
   }
 }
